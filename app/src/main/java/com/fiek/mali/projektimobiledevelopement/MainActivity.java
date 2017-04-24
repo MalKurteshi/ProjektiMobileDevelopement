de6f8ead68db161e63fde471b5c6ca2f3279103e
MalKurteshi <malkurteshi@gmail.com>
2017-04-24 15:13:42 +0200

    Aktiviteti Katet
    Lidhja mes main ne akticviti katet nepermes aktivitetit main

--- app/src/main/java/com/fiek/mali/projektimobiledevelopement/MainActivity.java
+++ app/src/main/java/com/fiek/mali/projektimobiledevelopement/MainActivity.java
@@ -1,13 +1,28 @@
 package com.fiek.mali.projektimobiledevelopement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnVazhdoPaLogim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnVazhdoPaLogim = (Button) findViewById(R.id.btnVazhdoPaLogim);
+
+        btnVazhdoPaLogim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent objKaloNeKate = new Intent(getApplicationContext(), Katet.class);
+                startActivity(objKaloNeKate);
+            }
+        });
     }
 }
